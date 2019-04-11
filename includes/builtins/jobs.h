997fd043721d23d8c38253b4bbcb2cc9ae107ee1
mmousson <mmousson@student.42.fr>
2019-04-11 20:39:07 +0200

    'jobs'builtin usage is still left to do

--- includes/builtins/jobs.h
+++ includes/builtins/jobs.h
@@ -6,7 +6,7 @@
 /*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 19:38:08 by mmousson          #+#    #+#             */
-/*   Updated: 2019/04/11 20:01:00 by mmousson         ###   ########.fr       */
+/*   Updated: 2019/04/11 20:31:08 by mmousson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@
 # include <stdlib.h>
 # include "libft.h"
 # include "job_control_42.h"
+
+# define ALLOWED_OPTIONS "lp"
+
+# define JOBS_OK 0
+# define JOBS_ERROR 1
+# define JOBS_INVALID_OPTION 2
 
 #endif
--- src/builtins/jobs.c
+++ src/builtins/jobs.c
@@ -6,23 +6,70 @@
 /*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 19:34:12 by mmousson          #+#    #+#             */
-/*   Updated: 2019/04/11 20:06:30 by mmousson         ###   ########.fr       */
+/*   Updated: 2019/04/11 20:38:45 by mmousson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "jobs.h"
 
-int	jobs(int argc, char **argv, char ***env)
+static void	usage(char c)
+{
+
+}
+
+static int	option_index(char c)
+{
+	int i;
+
+	i = 0;
+	while (ALLOWED_OPTIONS[i] != '\0')
+	{
+		if (c == ALLOWED_OPTIONS[i])
+			return (i);
+	}
+	return (-1);
+}
+
+static int	parse_options(int argc, char **argv)
+{
+	int	i;
+	int	index;
+	int	ret;
+
+	argc--;
+	argv++;
+	ret = 0;
+	while (argc > 0 && !ft_strequ(argv[0], "--"))
+	{
+		i = 0;
+		if (argv[0][0] != '-')
+			break;
+		while (argv[0][++i] != '\0')
+		{
+			if ((index = option_index(argv[0][i])) == -1)
+			{
+
+				return (-1);
+			}
+			else
+				ret |= 1 << index;
+		}
+	}
+	return (ret);
+}
+
+int			jobs(int argc, char **argv, char ***env)
 {
 	int		i;
+	int		options;
 	t_job	*current;
 
-	(void)argc;
-	(void)argv;
 	(void)env;
 	first_job_set_and_get(&current, GET);
 	if (current == NULL)
-		return (0);
+		return (JOBS_OK);
+	if ((options = parse_options(argc, argv)) == -1)
+		return (JOBS_INVALID_OPTION);
 	i = 1;
 	ft_putendl("Job\tGroup\tState\tCommand");
 	while (current)
@@ -35,5 +82,5 @@ int	jobs(int argc, char **argv, char ***env)
 		ft_putendl(current->command);
 		current = current->next;
 	}
-	return (0);
+	return (JOBS_OK);
 }
