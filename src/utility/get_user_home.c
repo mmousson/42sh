9aac2189e3db8ebd1af41b7a590c58658434e900
mmousson <mmousson@student.42.fr>
2019-04-13 23:04:12 +0200

    Added 'get_user_home' utility function

---
+++ src/utility/get_user_home.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_user_home.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/13 22:56:44 by mmousson          #+#    #+#             */
+/*   Updated: 2019/04/13 23:03:33 by mmousson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pwd.h>
+#include <unistd.h>
+#include <uuid/uuid.h>
+#include <sys/types.h>
+
+/*
+**	This functions returns the Path to the HOME directory associated with
+**	the current USER ID
+**	Simply gets the effective user ID of the calling process
+**	with the 'geteuid' system call
+**	Once it has been done, translates this ID into the corresponding HOME dir
+*/
+
+char	*get_user_home(void)
+{
+	struct passwd	*user_pwd;
+	uid_t			user_id;
+
+	if ((user_id = geteuid()) == -1)
+	{
+		write(STDERR_FILENO, "geteuid Undefined Error", 23);
+		return(NULL);
+	}
+	else
+		return (getpwuid(user_id)->pw_dir);
+}
