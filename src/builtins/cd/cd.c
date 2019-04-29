f86122f16e6eb9f80ff13475ed32b4338b2dbe30
Theo DUVAL <tduval@student.42.fr>
2019-04-29 23:02:12 +0200

    almost done

--- src/builtins/cd/cd.c
+++ src/builtins/cd/cd.c
@@ -6,20 +6,21 @@
 /*   By: tduval </var/mail/tduval>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 08:37:24 by tduval            #+#    #+#             */
-/*   Updated: 2019/04/29 21:46:41 by tduval           ###   ########.fr       */
+/*   Updated: 2019/04/29 23:02:07 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "cd.h"
 #include "sh42.h"
 #include "libft.h"
 
-static int	get_home(char ***env)
+static char	*get_home(char ***env)
 {
 	char	*res;
 
-	res = ft_getenv("HOME");
-	return (res == NULL ? get_user_home() : res);
+	res = utility_get_env_var(env, "HOME");
+	return (res == NULL ? utility_get_user_home() : res);
 }
 
 static void	free_list(char **list)
@@ -27,7 +28,7 @@ static void	free_list(char **list)
 	int		i;
 
 	i = 0;
-	while (list[i])
+	while (list && list[i])
 	{
 		ft_strdel(&list[i]);
 		i++;
@@ -43,11 +44,12 @@ static char	*get_cur(char ***environ, char *dir)
 	int		i;
 
 	i = 0;
+	list = 0;
 	if (dir && (ft_strnequ(dir, "./", 2)
-				|| ft_strnequ(dir, "../", 2_
-				|| ft_strnequ(dir, "/", 1))))
+				|| ft_strnequ(dir, "../", 2)
+				|| ft_strnequ(dir, "/", 1)))
 		return (ft_strdup(dir));
-	if ((cdpath = ft_getenv(environ, "CDPATH")))
+	if ((cdpath = utility_get_env_var(environ, "CDPATH")))
 	{
 		if (!(list = ft_strsplit(cdpath, ':')))
 		{
@@ -82,30 +84,31 @@ int			cd(int argc, char **argv, char ***env)
 
 	dir = NULL;
 	cur = NULL;
-	if ((opts = get_opts(argv, &dir)) == -1)
+	if ((opts = cd_get_opts(argv, &dir)) == -1)
 	{
 		ft_putendl_fd("cd: error: too many arguments.", 2);
 		return (1);
 	}
 	if (dir == NULL)
 	{
-		if (!(dir = get_home(env)));
+		if (!(dir = get_home(env)))
 			return (1);
 	}
-	if (!(cur = get_cur))
+	if (!(cur = get_cur(env, dir)))
 		return (1);
 	if (cur[0] != '/')
 	{
 		tmp = ft_strdup(cur);
-		cur = ft_strjoin(ft_getenv(env, "PWD"), "/");
-		cur = ft_strjoin(ft_getenv(cur, tmp);
+		cur = ft_strjoin(utility_get_env_var(env, "PWD"), "/");
+		cur = ft_strjoin(cur, tmp);
 		ft_strdel(&tmp);
 	}
 	if (chdir(cur) != -1)
 	{
+		ft_putendl(cur);
 		if ((opts & OPT_L) == 0)
-			ft_setenv("PWD", cur);
-		else
+			utility_add_entry_to_environ(env, "PWD", cur);
+		//else
 			//TODO
 	}
 	return (0);
