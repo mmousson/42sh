5a2280cff3f06f8b317a6b3b2cfe5dc20c304fd1
Theo DUVAL <tduval@student.42.fr>
2019-04-29 23:01:51 +0200

    -- management

--- src/builtins/cd/cd_get_opts.c
+++ src/builtins/cd/cd_get_opts.c
@@ -6,13 +6,14 @@
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 21:52:00 by tduval            #+#    #+#             */
-/*   Updated: 2019/04/22 21:52:48 by tduval           ###   ########.fr       */
+/*   Updated: 2019/04/29 23:01:35 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cd.h"
 #include "libft.h"
 
-char     get_opts(char **argv, char **dir)
+char	cd_get_opts(char **argv, char **dir)
 {
 	char    res;
 	char    cur;
@@ -21,29 +22,29 @@ char     get_opts(char **argv, char **dir)
 
 	i = 1;
 	res = 0;
-	while (argv[i] && argv[i][0] == '-' && argv[i][1] != '\0')
+	while (argv[i] && argv[i][0] == '-' && argv[i][1] != '\0' && ft_strcmp(argv[i], "--"))
 	{
-			j = 1;
-			cur = res;
-			while (argv[i][j])
+		j = 1;
+		cur = res;
+		while (argv[i][j])
+		{
+			if (argv[i][j] == 'L')
+				res |= OPT_L;
+			else if (argv[i][j] == 'P')
+				res |= OPT_P;
+			else
 			{
-					if (argv[i][j] == 'L')
-							res |= OPT_L;
-					else if (argv[i][j] == 'P')
-							res |= OPT_P;
-					else
-					{
-							if (argv[i + 1] != NULL)
-									return (-1);
-							*dir = ft_strdup(argv[i]);
-							return (cur);
-					}
-					j++;
+				if (argv[i + 1] != NULL)
+					return (-1);
+				*dir = ft_strdup(argv[i]);
+				return (cur);
 			}
-			i++;
+			j++;
+		}
+		i++;
 	}
 	if (argv[i] != NULL && argv[i + 1] != NULL)
-			return (-1);
+		return (-1);
 	*dir = ft_strdup(argv[i]);
 	return (res);
 }
