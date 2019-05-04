7045c560decb3253c28c4b4e639f4200caad3a29
Theo DUVAL <tduval@student.42.fr>
2019-05-04 03:06:56 +0200

    normed

--- src/builtins/cd/cd_get_opts.c
+++ src/builtins/cd/cd_get_opts.c
@@ -6,41 +6,60 @@
 /*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/22 21:52:00 by tduval            #+#    #+#             */
-/*   Updated: 2019/04/30 02:16:57 by tduval           ###   ########.fr       */
+/*   Updated: 2019/05/04 03:06:38 by tduval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cd.h"
 #include "libft.h"
 
-char	cd_get_opts(char **argv, char **dir)
+/*
+**	Basically the parser of the cd builtin.
+**	It will first take the options, then once it encouters takes the next
+**	arg as the directory variable. If there is any argument after the
+**	directory one, then it is considered as an error.
+*/
+
+static char	check_arg(char *arg, char *next, char **dir, char cur)
 {
-	char    res;
-	char    cur;
+	char	res;
+	int		i;
+
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i] == 'L')
+			res = OPT_L;
+		else if (arg[i] == 'P')
+			res = OPT_P;
+		else
+		{
+			if (next != NULL)
+				return (-1);
+			*dir = ft_strdup(arg);
+			return (cur);
+		}
+		i++;
+	}
+	return (res);
+}
+
+char		cd_get_opts(char **argv, char **dir)
+{
+	char	res;
+	char	cur;
 	int		i;
 	int		j;
 
 	i = 1;
 	res = 0;
-	while (argv[i] && argv[i][0] == '-' && argv[i][1] != '\0' && ft_strcmp(argv[i], "--"))
+	while (argv[i] && argv[i][0] == '-'
+			&& argv[i][1] != '\0'
+			&& ft_strcmp(argv[i], "--"))
 	{
-		j = 1;
 		cur = res;
-		while (argv[i][j])
-		{
-			if (argv[i][j] == 'L')
-				res = OPT_L;
-			else if (argv[i][j] == 'P')
-				res = OPT_P;
-			else
-			{
-				if (argv[i + 1] != NULL)
-					return (-1);
-				*dir = ft_strdup(argv[i]);
-				return (cur);
-			}
-			j++;
-		}
+		if ((res = check_arg(argv[i], argv[i + 1], dir, cur)) == -1)
+			return (-1);
 		i++;
 	}
 	if (ft_strequ(argv[i], "--"))
@@ -50,4 +69,3 @@ char	cd_get_opts(char **argv, char **dir)
 	*dir = argv[i];
 	return (res);
 }
-
