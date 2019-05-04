bb35ebdfc38b39756b47bcb956ef516fc734d70e
Theo DUVAL <tduval@student.42.fr>
2019-05-04 02:51:44 +0200

    renamed

---
+++ src/builtins/cd/cd_get_pwd.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_pwd.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/03 21:18:38 by tduval            #+#    #+#             */
+/*   Updated: 2019/05/04 02:18:23 by tduval           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	free_files(char **files)
+{
+	int		i;
+
+	i = 0;
+	while (files && files[i])
+	{
+		ft_strdel(&files[i]);
+		i++;
+	}
+	ft_memdel((void **)&files);
+}
+
+static char	**delete_last(char **files, int i)
+{
+	while (i && ft_strequ(files[i], ""))
+		i--;
+	ft_strdel(&files[i]);
+	files[i] = ft_strdup("");
+	return (files);
+}
+
+static char	**change_files(char **files, int f)
+{
+	int		i;
+
+	i = 0;
+	while (files && files[i] && f == 1)
+	{
+		if (files[i] && ft_strcmp(files[i], ".") == 0)
+		{
+			ft_strdel(&files[i]);
+			files[i] = ft_strdup("");
+		}
+		i++;
+	}
+	while (files && files[i] && f == 2)
+	{
+		if (ft_strcmp(files[i], "..") == 0)
+		{
+			ft_strdel(&files[i]);
+			files[i] = ft_strdup("");
+			if (i)
+				files = delete_last(files, i);
+			i = -1;
+		}
+		i++;
+	}
+	return (files);
+}
+
+static char	*get_res(char **files)
+{
+	char	*res;
+	char	*tmp;
+	int		i;
+
+	i = 0;
+	res = 0;
+	while (files && files[i])
+	{
+		if (res == 0 && ft_strcmp(files[i], ""))
+		{
+			tmp = ft_strdup("/");
+			res = ft_strjoin(tmp, files[i]);
+			ft_strdel(&tmp);
+		}
+		else if (ft_strcmp(files[i], ""))
+		{
+			tmp = ft_strjoin(res, "/");
+			ft_strdel(&res);
+			res = ft_strjoin(tmp, files[i]);
+			ft_strdel(&tmp);
+		}
+		i++;
+	}
+	return (res ? res : ft_strdup("/"));
+}
+
+char		*get_pwd(char *cur)
+{
+	char	**files;
+	char	*res;
+	int		i;
+
+	i = 0;
+	files = NULL;
+	res = NULL;
+	files = ft_strsplit(cur, '/');
+	files = change_files(files, 1);
+	res = get_res(files);
+	free_files(files);
+	files = ft_strsplit(res, '/');
+	files = change_files(files, 2);
+	ft_strdel(&res);
+	res = get_res(files);
+	free_files(files);
+	return (res);
+}
