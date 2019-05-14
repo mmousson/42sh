1b6273ba4da5092ea6388eae7da720b404c55f9f
mmousson <mmousson@student.42.fr>
2019-05-14 06:17:00 +0200

    Documented and Normed the new files

--- src/utility/utility_get_var.c
+++ src/utility/utility_get_var.c
@@ -6,29 +6,54 @@
 /*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/14 04:57:56 by mmousson          #+#    #+#             */
-/*   Updated: 2019/05/14 05:07:34 by mmousson         ###   ########.fr       */
+/*   Updated: 2019/05/14 06:16:12 by mmousson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "sh42.h"
 
-static char *get_var_from_environment(char *name, char ***env)
+/*
+**	We first look for the variable 'name' in the shell's environment
+**
+**	Arguments:
+**	name -> The name of the variable we are looking for
+**	env -> The shell's current environment array
+**
+**	Return Value:
+**	NULL -> The variable doesn't exist
+**	NON-NULL -> The value of the variable designated by 'name'
+*/
+
+static char	*get_var_from_environment(char *name, char **env)
 {
 	int		i;
 	size_t	name_len;
 
 	i = -1;
 	name_len = ft_strlen(name);
-	while ((*env)[++i] != NULL)
+	while (env[++i] != NULL)
 	{
-		if (ft_strnequ(name, (*env)[i], name_len) && (*env)[i][name_len] == '=')
-			return (ft_strdup((*env)[i] + name_len + 1));
+		if (ft_strnequ(name, env[i], name_len) && env[i][name_len] == '=')
+			return (ft_strdup(env[i] + name_len + 1));
 	}
 	return (NULL);
 }
 
-static char *get_var_from_internal_definitions(char *name)
+/*
+**	If the variable doesn't exist in the shell's environment, we try to
+**	retrieve it from the linked list 'g_shell_var_list'
+**
+**	Arguments:
+**	name -> The name of the variable we are looking for
+**	env -> The shell's current environment array
+**
+**	Return Value:
+**	NULL -> The variable doesn't exist
+**	NON-NULL -> The value of the variable designated by 'name'
+*/
+
+static char	*get_var_from_internal_definitions(char *name)
 {
 	t_vars	*current;
 
@@ -42,13 +67,28 @@ static char *get_var_from_internal_definitions(char *name)
 	return (NULL);
 }
 
-char        *utility_get_var(char *name, char ***env)
-{
-    char    *res;
+/*
+**	This wrapper function returns the value, if it exists, of the variable
+**	designated by 'name'
+**	It searches both in the environment and in the shell's linked list
+**	'g_shell_var_list'
+**
+**	Arguments:
+**	name -> The name of the variable we are looking for
+**	env -> The shell's current environment array
+**
+**	Return Value:
+**	NULL -> The variable doesn't exist
+**	NON-NULL -> The value of the variable designated by 'name'
+*/
 
-    if ((res = get_var_from_environment(name, env)) != NULL
-        || (res = get_var_from_internal_definitions(name)) != NULL)
-        return (res);
-    else
-        return (NULL);
+char		*utility_get_var(char *name, char **env)
+{
+	char	*res;
+
+	if ((res = get_var_from_environment(name, env)) != NULL
+		|| (res = get_var_from_internal_definitions(name)) != NULL)
+		return (res);
+	else
+		return (NULL);
 }
--- src/utility/utility_set_var.c
+++ src/utility/utility_set_var.c
@@ -6,13 +6,28 @@
 /*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 06:20:40 by mmousson          #+#    #+#             */
-/*   Updated: 2019/05/14 04:56:08 by mmousson         ###   ########.fr       */
+/*   Updated: 2019/05/14 06:04:10 by mmousson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 #include "sh42.h"
+
+/*
+**	This simple function generates a string holding the information given by
+**	'name' and 'value' in the form:
+**	%s=%s, name, value
+**	In case of memory allocation error, NULL is returned
+**
+**	Arguments:
+**	name -> First part of the string
+**	value -> Second part of the string
+**
+**	Return Value:
+**	NULL -> A memory allocation failed
+**	NON-NULL -> The generated string
+*/
 
 static char	*generate_env_definition(char *name, char *value)
 {
@@ -32,7 +47,26 @@ static char	*generate_env_definition(char *name, char *value)
 	return (res);
 }
 
-static int	set_already_existing_env_var(char *name, char *value, char ***env)
+/*
+**	First function to be called by the 'utility_set_var' wrapper function
+**	It searches for a variable designed by 'name' in the 'env' array
+**	If found, it will replace that array element by a new one, with the
+**	same name, but with the 'value' content in the form:
+**	%s=%s\n, name, value
+**	In case of memory allocation error, nothing is modified in the 'env' array
+**
+**	Arguments:
+**	name -> The name of the variable to replace, if it exists in env
+**	value -> The new content of the variable 'name', if it exists in env
+**	env -> The shell's current environment array
+**
+**	Return Value:
+**	0 -> A variable designed by 'name' doesn't exist in the 'env' array
+**	1 -> The variable designed by 'name' was found in the 'env' array
+**		and its content was replaced by 'value'
+*/
+
+static int	set_already_existing_env_var(char *name, char *value, char **env)
 {
 	int		i;
 	size_t	name_len;
@@ -40,16 +74,16 @@ static int	set_already_existing_env_var(char *name, char *value, char ***env)
 
 	i = -1;
 	name_len = ft_strlen(name);
-	while ((*env)[++i] != NULL)
+	while (env[++i] != NULL)
 	{
-		if (ft_strnequ(name, (*env)[i], name_len) && (*env)[i][name_len] == '=')
+		if (ft_strnequ(name, env[i], name_len) && env[i][name_len] == '=')
 		{
 			if ((tmp = generate_env_definition(name, value)) == NULL)
 				ft_putendl_fd("42sh: Internal Malloc Error", STDERR_FILENO);
 			else
 			{
-				ft_strdel(&((*env)[i]));
-				(*env)[i] = tmp;
+				ft_strdel(&(env[i]));
+				env[i] = tmp;
 			}
 			return (1);
 		}
@@ -57,7 +91,25 @@ static int	set_already_existing_env_var(char *name, char *value, char ***env)
 	return (0);
 }
 
-static int	set_already_existing_internal_var(char *name)
+/*
+**	Second function to be called by the 'utility_set_var' wrapper function
+**	It searches for a variable definition with the name 'name' in the
+**	'g_shell_var_list' linked list
+**	If found, it replaces its field 'value' by the 'value' argument
+**	In case of memory allocation error, the link is remove from the list
+**	and all the memory it occupies is free'd
+**
+**	Arguments:
+**	name -> The name of the variable to look for in the 'g_shell_var_list'
+**	value -> The replacement value of the wanted variable definition
+**
+**	Return Value:
+**	0 -> A variable designed by 'name' doesn't exist in the linked list
+**	1 -> The variable designed by 'name' was found in the linked list
+**		and its content field was replaced by 'value'
+*/
+
+static int	set_already_existing_internal_var(char *name, char *value)
 {
 	t_vars	*prev;
 	t_vars	*current;
@@ -69,12 +121,11 @@ static int	set_already_existing_internal_var(char *name)
 		if (ft_strequ(name, current->name))
 		{
 			ft_strdel(&current->name);
-			if ((current->name = ft_strdup(name)) == NULL)
+			if ((current->name = ft_strdup(value)) == NULL)
 			{
 				ft_putendl_fd("42sh: Internal Malloc Error", STDERR_FILENO);
-				if (prev != NULL)
-					prev->next = current->next;
-				else
+				(prev != NULL) ? prev->next = current->next : (0);
+				if (prev == NULL)
 					g_shell_var_list = current->next;
 				ft_strdel(&current->value);
 				ft_memdel((void **)&(current));
@@ -86,6 +137,22 @@ static int	set_already_existing_internal_var(char *name)
 	}
 	return (0);
 }
+
+/*
+**	In case both of the function above return 0 (which means the variable
+**	designed by 'name' doesn't exist yet), this function is called by the
+**	'utility_set_var' wrapper function
+**	It creates a new link and fills it with the value given by the arguments
+**	'name' and 'value' and then prepends the link to the current linked list,
+**	updating its head pointer
+**	In case of memory allocation error, nothing is modified
+**
+**	Arguments:
+**	name -> The name of the variable to create in the linked list
+**	value -> The value of the wanted variable definition
+**
+**	Return Value: NONE
+*/
 
 static void	add_var(char *name, char *value)
 {
@@ -113,13 +180,33 @@ static void	add_var(char *name, char *value)
 	}
 }
 
-void		utility_set_var(char *name, char *value, char ***env)
+/*
+**	This functions sets a variable in the current shell's environment or as an
+**	internal variable
+**	It does so by modifying an already existing defintion or creating a new one
+**	when appropriate
+**	It first checks if such a definition with the same name already exists in
+**	the environment and if so, modifies it
+**	If not, it then searches a defintion with the same name in the internal
+**	variables list and if so, modifies it
+**	If both condition are not met, it creates a freshly new definition and
+**	prepends it to the 'g_shell_var_list'
+**
+**	Arguments:
+**	name -> The name of the variable we want to set
+**	value -> The value of the variable we want to set
+**	env -> A pointer to the shell's current environment
+**
+**	Return Value: NONE
+*/
+
+void		utility_set_var(char *name, char *value, char **env)
 {
 	if (name == NULL || value == NULL || env == NULL)
 		return ;
 	if (set_already_existing_env_var(name, value, env) == 1)
 		return ;
-	else if (set_already_existing_internal_var(name) == 1)
+	else if (set_already_existing_internal_var(name, value) == 1)
 		return ;
 	else
 		add_var(name, value);
