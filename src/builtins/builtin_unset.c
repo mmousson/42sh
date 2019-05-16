e1d997211577033a96fda4f24705ae564a76bec0
mmousson <malomallory@hotmail.fr>
2019-05-16 16:04:42 +0200

    Reoworked 'unset' utility to work with the new variable manmagement system

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -6,65 +6,11 @@
 /*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/25 01:51:39 by mmousson          #+#    #+#             */
-/*   Updated: 2019/05/14 04:46:49 by mmousson         ###   ########.fr       */
+/*   Updated: 2019/05/16 16:02:50 by mmousson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "unset.h"
-
-/*
-**	This function will delete a variable 'name'
-**	from the shell's internal variables list
-**
-**	Arguments:
-**	name -> The variable's name to look for
-**
-**	Return Value: NONE
-*/
-
-static void	search_in_shell_internal_vars(char *name)
-{
-	t_vars	*current;
-	t_vars	*prev;
-
-	prev = NULL;
-	current = g_shell_var_list;
-	while (current)
-	{
-		if (ft_strequ(name, current->name))
-		{
-			if (prev == NULL)
-				g_shell_var_list = current->next;
-			else
-				prev->next = current->next;
-			ft_strdel(&current->name);
-			ft_strdel(&current->value);
-			ft_memdel((void **)&(current));
-		}
-		prev = current;
-		current = current->next;
-	}
-}
-
-/*
-**	This functions will delete a variable 'name'
-**	from the shell's environment
-**
-**	Arguments:
-**	name -> The variable's name to look for
-**	env -> The shell's environment array
-**
-**	Return Value: NONE
-*/
-
-static void	search_in_environment(char *name, char ***env)
-{
-	int	i;
-
-	i = -1;
-	while ((*env)[++i] != NULL)
-		utility_rm_entry_from_environ(env, name);
-}
 
 /*
 **	============================= BUITLIN COMMAND =============================
@@ -82,9 +28,6 @@ int			blt_unset(int argc, char **argv, char ***env)
 
 	i = 0;
 	while (++i < argc)
-	{
-		search_in_environment(argv[i], env);
-		search_in_shell_internal_vars(argv[i]);
-	}
+		utility_delete_var(argv[i], env);
 	return (0);
 }
