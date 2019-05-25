a5e1402833566bd69ff4c4cedec05b917eeef7cf
mmousson <mmousson@student.42.fr>
2019-05-25 11:08:56 +0200

    Switched new tmp file generation on random

--- src/utility/utility_generate_tmp_filename.c
+++ src/utility/utility_generate_tmp_filename.c
@@ -6,10 +6,11 @@
 /*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/25 10:20:08 by mmousson          #+#    #+#             */
-/*   Updated: 2019/05/25 10:45:33 by mmousson         ###   ########.fr       */
+/*   Updated: 2019/05/25 11:07:29 by mmousson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/random.h>
 #include "sh42.h"
 #include "libft.h"
 
@@ -19,40 +20,35 @@
 **
 **	Arguments:
 **	base -> The current combination
-**	new_suffix -> The new number of the file
 **
 **	Return Value:
 **	NULL -> A malloc error occured
 **	NON-NULL -> The next permutation
 */
 
-static char	*next_permutation(const char *base, char new_suffix)
+static char	*next_permutation(const char *base)
 {
 	size_t	len;
 	char	*new;
+	char	next_suffix;
 
 	len = ft_strlen(base);
-	if ((new = ft_strnew(len + (new_suffix == '0' + 10))) == NULL)
+	if ((new = ft_strnew(len + 1)) == NULL)
 	{
 		ft_putendl_fd("42sh: Internal Malloc Error", STDERR_FILENO);
 		return (NULL);
 	}
 	ft_strcpy(new, base);
-	if (new_suffix == '0' + 10)
-	{
-		new[len - 1] = '_';
-		new[len] = '1';
-	}
-	else
-		new[len - 1] = new_suffix;
+	getentropy(&next_suffix, 1);
+	new[len] = (ft_abs(next_suffix) % 40) + 42;
 	return (new);
 }
 
 /*
 **	Function that generates a valid and available filename in order for it to
 **	be used as a temporary file (heredoc AND/OR substitutions)
-**	It generates a path with a base '.tmp' followed by a '_' padding and a
-**	number
+**	It generates a path with a base '.tmp' followed by a new random character
+**		each time
 **	Each generated combination is then used to verify whether a file pointed
 **	by 'res' already exists and if not, the result is returned
 **
@@ -64,25 +60,23 @@ static char	*next_permutation(const char *base, char new_suffix)
 **		created and used
 */
 
-char		*utility_generate_filename(void)
+char		*utility_generate_tmp_filename(void)
 {
 	char	*res;
 	char	*tmp;
-	int		i;
 
 	if ((res = ft_strdup(".tmp_")) != NULL)
 	{
-		i = 0;
 		while (42)
 		{
-			if ((tmp = next_permutation(res, '0' + i)) == NULL)
+			if ((tmp = next_permutation(res)) == NULL)
 				return (NULL);
+			printf("Got: %s\n", tmp);
+			usleep(60000);
 			ft_strdel(&res);
 			res = tmp;
 			if (!utility_file_exists(res))
 				return (res);
-			i %= 10;
-			i++;
 		}
 	}
 	else
