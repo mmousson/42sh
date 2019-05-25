31166e07a1945a45d476ea333d295615a8106e3f
Robin Oliveira <robin.10contact@gmail.com>
2019-05-25 18:58:15 +0200

    libft
    ft_strcmp protected

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,12 +6,16 @@
 /*   By: mmousson <mmousson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 01:36:24 by mmousson          #+#    #+#             */
-/*   Updated: 2019/02/19 20:25:03 by mmousson         ###   ########.fr       */
+/*   Updated: 2019/05/25 18:57:24 by roliveir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
+	if (!s1 && !s2)
+		return (0);
+	if (!s1 || !s2)
+		return (1);
 	while (*s1 == *s2 && *s1 && *s2)
 	{
 		s1++;
