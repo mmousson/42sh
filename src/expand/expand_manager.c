8719e1140892c1d41a1747ffe944fc6a438dbb39
octave <octave.boutrolle@gmail.com>
2019-05-28 10:09:04 +0200

    temporary expand for globbing

--- src/expand/expand_manager.c
+++ src/expand/expand_manager.c
@@ -6,13 +6,26 @@
 /*   By: oboutrol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 14:52:12 by oboutrol          #+#    #+#             */
-/*   Updated: 2019/05/25 11:27:28 by oboutrol         ###   ########.fr       */
+/*   Updated: 2019/05/28 10:08:24 by oboutrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expand.h"
 
 #include "libft.h"
+static int	expand_all_filename(char ***argv)
+{
+	int		k;
+
+	k = -1;
+	while ((*argv)[++k])
+	{
+		if (expand_filename(&(*argv)[k]))
+			return (1);
+	}
+	return (0);
+}
+
 int		expand_manager(char ***argv, char ***arge)
 {
 	int	ret;
@@ -31,8 +44,8 @@ int		expand_manager(char ***argv, char ***arge)
 	//
 	//if (expand_word_splitting)
 	//
-	//if (expand_filename)
-	//
+	if (expand_all_filename(argv))
+		return (1);
 	if (expand_quote_removal(argv))
 		return (1);
 	return (0);
