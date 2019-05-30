14b45b81333a70dfee3fb1ff4df9be913e624cf0
octave <octave.boutrolle@gmail.com>
2019-05-30 17:46:34 +0200

    with the right expand

--- src/expand/expand_param.c
+++ src/expand/expand_param.c
@@ -6,11 +6,12 @@
 /*   By: oboutrol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 14:27:00 by oboutrol          #+#    #+#             */
-/*   Updated: 2019/05/28 18:35:43 by oboutrol         ###   ########.fr       */
+/*   Updated: 2019/05/30 17:44:21 by oboutrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exe.h"
+#include "sh42.h"
 #include "expand.h"
 #include "libft.h"
 #include <stdlib.h>
@@ -37,7 +38,7 @@ char		*expand_param(const char *str, char ***arge, int *end)
 		k++;
 	word = ft_strsub(str, 1, k - 1);
 	if (!(value = get_spec_param(&(str[1]))))
-		value = ft_getenv(*arge, word);
+		value = utility_get_var(word, *arge);
 	ft_strdel(&word);
 	*end = k - 1;
 	if (str[1] == '?' || str[1] == '$' || str[1] == '!')
