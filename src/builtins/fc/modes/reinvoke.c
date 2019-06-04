00b95b38cb131e2fb689581616f0511abf8bdf35
mmousson <mmousson@student.42.fr>
2019-06-05 01:02:12 +0200

    'fc' direct re-invokation OK

--- src/builtins/fc/modes/reinvoke.c
+++ src/builtins/fc/modes/reinvoke.c
@@ -14,6 +14,54 @@
 #include "lex.h"
 
 /*
+**	Because of the NORM --"
+*/
+
+static char	*malloc_error(char *to_del)
+{
+	if (to_del != NULL)
+		ft_strdel(&to_del);
+	ft_putendl_fd("42sh: Internal Malloc Error", STDERR_FILENO);
+	return (NULL);
+}
+
+/*
+**	This function generates the actual new command to be re-invoked, taking
+**	the 'old=new' operand into account, if any
+**
+**	Arguments:
+**	inf -> A pointer to the Data-Structure holding all informations about
+**		the current 'fc' utility instance
+**
+**	Return Value:
+**	NULL -> An Internal Malloc Error Occured
+**	NON-NULL -> The fresh new command
+*/
+
+static char	*get_fresh_command(t_options_infos *inf)
+{
+	size_t	first_len;
+	size_t	old_len;
+	char	*catch;
+	char	*fresh;
+
+	catch = ft_strstr(inf->from->line, inf->dash_s->old);
+	if (!(fresh = ft_strsub(inf->from->line, 0, catch - inf->from->line)))
+		return (malloc_error(NULL));
+	first_len = ft_strlen(fresh);
+	old_len = ft_strlen(inf->dash_s->old);
+	if ((catch = ft_strjoin(fresh, inf->dash_s->new)) == NULL)
+		return (malloc_error(fresh));
+	ft_strdel(&fresh);
+	fresh = catch;
+	if (!(catch = ft_strjoin(fresh, inf->from->line + old_len + first_len)))
+		return (malloc_error(fresh));
+	ft_strdel(&fresh);
+	fresh = catch;
+	return (fresh);
+}
+
+/*
 **	This function directly reinvokes a command from the history without
 **	editing it
 **	If an operand of the form 'old=new' has been provided to the utility,
@@ -34,26 +82,18 @@
 int	blt_fc_reinvoke(t_options_infos *inf, char ***env)
 {
 	int		ret;
-	size_t	first_len;
-	size_t	old_len;
-	char	*catch;
 	char	*fresh;
 
 	if (inf->dash_s->old != NULL && inf->dash_s->new != NULL)
-	{
-		catch = ft_strstr(inf->from->line, inf->dash_s->old);
-		fresh = ft_strsub(inf->from->line, 0, catch - inf->from->line);
-		first_len = ft_strlen(fresh);
-		old_len = ft_strlen(inf->dash_s->old);
-		catch = ft_strjoin(fresh, inf->dash_s->new);
-		ft_strdel(&fresh);
-		fresh = catch;
-		catch = ft_strjoin(fresh, inf->from->line + old_len + first_len);
-		ft_strdel(&fresh);
-		fresh = catch;
-	}
+		fresh = get_fresh_command(inf);
 	else
-		fresh = inf->from->line;
+	{
+		if ((fresh = ft_strdup(inf->from->line)) == NULL)
+		{
+			ft_putendl_fd("42sh: Internal Malloc Error", STDERR_FILENO);
+			return (2);
+		}
+	}
 	ret = !lex_str(&fresh, env);
 	ft_strdel(&fresh);
 	return (ret);
