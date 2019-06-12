7c09184455d50abed66d66e1f5673035e77f5f60
octave <octave.boutrolle@gmail.com>
2019-06-12 03:54:59 +0200

    fix on doll

--- src/lexer/lex_store_dol.c
+++ src/lexer/lex_store_dol.c
@@ -6,7 +6,7 @@
 /*   By: oboutrol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 12:01:16 by oboutrol          #+#    #+#             */
-/*   Updated: 2019/05/31 15:33:16 by oboutrol         ###   ########.fr       */
+/*   Updated: 2019/06/12 03:19:28 by oboutrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ int			lex_store_dol(t_stat *stat, char buff[BUF], char **str)
 {
 	char	next_char;
 
-	next_char = (*str)[stat->k + 1];
-	if (ft_strchr("({", next_char))
+	if ((next_char = (*str)[stat->k + 1]) && ft_strchr("({", next_char))
 	{
 		lex_add_char(buff, &(stat->load), stat->cha);
 		stat->k += 1;
