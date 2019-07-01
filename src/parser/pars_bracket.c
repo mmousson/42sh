7bcb78344838e30fc84f138ae79711d0d4571bb7
octave <octave.boutrolle@gmail.com>
2019-07-01 14:44:16 +0200

    secure for quoting

--- src/parser/pars_bracket.c
+++ src/parser/pars_bracket.c
@@ -6,11 +6,32 @@
 /*   By: oboutrol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 11:41:42 by oboutrol          #+#    #+#             */
-/*   Updated: 2019/07/01 12:18:17 by oboutrol         ###   ########.fr       */
+/*   Updated: 2019/07/01 14:43:59 by oboutrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expand.h"
+
+int			is_esc(char c)
+{
+	static int	sq;
+	static int	dq;
+	static int	bs;
+
+	if (!c)
+	{
+		sq = 0;
+		dq = 0;
+		bs = 0;
+	}
+	if (!bs && c == '\\')
+		bs = 1;
+	if (!bs && !dq)
+		sq = c == '\'' ? -sq + 1 : sq;
+	if (!bs && !sq)
+		dq = c == '"' ? -dq + 1 : dq;
+	return (sq || dq || bs);
+}
 
 int			pars_bracket(t_tok **tok)
 {
@@ -18,23 +39,21 @@ int			pars_bracket(t_tok **tok)
 	int		k;
 	char	end;
 
-	k = 0;
 	if (!(*tok))
 		return (0);
 	line = (*tok)->content;
 	end = line[0] == '(' ? ')' : 0;
+	is_esc(0);
 	if (!end)
 		end = line[0] == '{' ? '}' : 0;
-	while (line[k] && line[k] != end)
-	{
-		if (!end && (line[k] == '{' || line [k] == '('))
+	k = -1;
+	while (line[++k] && line[k] != end)
+		if (!end && !is_esc(line[k]) && (line[k] == '{' || line [k] == '('))
 		{
 			ft_putstr_fd("42sh: Parse Error\n", 2);
 			return (1);
 		}
-		k++;
-	}
-	if (line[k] && line[k +1])
+	if (k >= 0 &&  line[k] && line[k +1])
 	{
 		ft_putstr_fd("42sh: Parse Error\n", 2);
 		return (1);
